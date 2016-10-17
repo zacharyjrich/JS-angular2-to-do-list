a503b3812bed168877eeb5f25f436c8637b75f10
Stephen Emery and Zachary Richards <stephen@>
2016-10-17 14:06:04 -0700

    Add form.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,16 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 (click)="doStuff()" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <h3 (click)="showDetails(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <h1>Edit Task</h1>
+    <div>
+      <label> Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+    </div>
+    <div>
+      <label> Enter Task ID:</label>
+      <input [(ngModel)]="selectedTask.id">
+    </div>
   </div>
   `
 })
@@ -17,12 +26,13 @@ export class AppComponent {
     new Task("Rewatch all the Lord of the Rings movies.", 2),
     new Task("Do the laundry.", 3)
   ];
-  doStuff() {
-    alert("You can click!");
+  selectedTask: Task = this.tasks[0];
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string, public is: number) { }
+  constructor(public description: string, public id: number) { }
 }
