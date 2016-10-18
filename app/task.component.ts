7f3aa66fca605ff965a0cc03fc32c367f6f8d500
Stephen Emery and Zachary Richards <zachary@>
2016-10-18 11:30:16 -0700

    Add priorities to Task Class.

--- app/task.component.ts
+++ app/task.component.ts
@@ -7,7 +7,7 @@ import { Task } from './task.model';
   <div>
     <input *ngIf="task.done === true" type="checkbox" checked (click)="toggleDone(false)"/>
     <input *ngIf="task.done === false" type="checkbox" (click)="toggleDone(true)"/>
-    <label>{{ task.description }}</label>
+    <label>{{ task.description }} {{ task.priority }}</label>
   </div>
   `
 })
--- app/task.model.ts
+++ app/task.model.ts
@@ -1,4 +1,5 @@
 export class Task {
   public done: boolean = false;
+  public priority: string = "High";
   constructor(public description: string, public id: number) { }
 }
